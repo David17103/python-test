ebeccdf99d4e9b3c144f46cac0d537c0b3ce16b7
David17103 <davida.chetty12@gmail.com>
2023-06-06 01:43:33 -0700

    Testing Task 0 code

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+number1 = random.randint (1, 10)
+number2 = random.randint (-1, -10)
+if number>0:
+    print(number1)
+elif number==0:
+    print(0)
+else:
+    print(number2)
