253ce29a65ee1c6a6101f18ff3a395c08573d8aa
David17103 <davida.chetty12@gmail.com>
2023-06-08 08:08:42 -0700

    task 2 requirement update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     argv = sys.argv[1:]
     list_argv = len(argv)
 
-    print("{} argument{}:".format(list_argv, "s" if list_argv !=1 else ""))
+    print("{} argument{}:".format(list_argv, "s" if list_argv !=1 else "") end="")
+    print('.' if list_argv == 0 else '')
 
     for i, arg in enumerate(argv, start=1):
         print("{} {}".format(i, arg))
