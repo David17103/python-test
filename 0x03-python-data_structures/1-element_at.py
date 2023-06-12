505c77d4b68cc2fb74edac828e761446a0bfd42a
David17103 <davida.chetty12@gmail.com>
2023-06-12 04:15:01 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return: "None"
-    else idx > my_list:
-        return: "None"
+if __name__ == '__main__':
+    def element_at(my_list, idx):
+        if idx < 0:
+            return: "None"
+        else idx > my_list:
+            return: "None"
