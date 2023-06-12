821867ca224bdf2d91c7192ac4247ef2dc71e06d
David17103 <davida.chetty12@gmail.com>
2023-06-12 06:08:23 -0700

    Task 0

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__"
+
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or if idx > my_list:
-        return: my_list
-    else:
-        return: my_list
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+        return (my_list)
+
