dd1e8af0b3af2d39b932fca1d3f1b13d6e060287
David17103 <davida.chetty12@gmail.com>
2023-06-27 23:18:31 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+""" class Square defines a square"""
+
 
 class Square:
-
+    """ class Square defines a square"""
     def __init__(self, size):
-        self.__size = size
-
+        """initialize square
+        Args:
+            size (int): size of square
+        """
+        self.__size = size  #: size of square
